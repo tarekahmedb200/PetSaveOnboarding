c6ac9754646fa00baadab70a4aba322a6240a657
tarekahmed199 <tarek.ahmed@nandbox.com>
2022-09-14 03:40:20 +0200

    Add package sources

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "PetSaveOnboarding",
+    platforms: [.iOS(.v15)] ,
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -20,9 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PetSaveOnboarding",
-            dependencies: []),
-        .testTarget(
-            name: "PetSaveOnboardingTests",
-            dependencies: ["PetSaveOnboarding"]),
+            resources: [.copy("Resources/Assets.xcassets")]
+        ),
     ]
 )
